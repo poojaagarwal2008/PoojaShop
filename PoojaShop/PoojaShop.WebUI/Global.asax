ac81ebecb15999aef060b78be8f6af3329bbbe35
poojaagarwal2008 <poojaagarwal2008@gmail.com>
2021-08-05 00:45:26 +0530

    Initial Solution Setup

---
+++ PoojaShop/PoojaShop.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PoojaShop.WebUI.MvcApplication" Language="C#" %>
---
+++ PoojaShop/PoojaShop.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PoojaShop.WebUI.Startup))]
+namespace PoojaShop.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
