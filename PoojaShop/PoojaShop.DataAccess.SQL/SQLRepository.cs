8fb7beccc066b9a42aefd31d9ac926ae5b4051c7
poojaagarwal2008 <poojaagarwal2008@gmail.com>
2021-08-17 01:22:47 +0530

    Adding SQL Repository and using SQL to store data

---
+++ PoojaShop/PoojaShop.DataAccess.SQL/SQLRepository.cs
@@ -0,0 +1,61 @@
+﻿using PoojaShop.Core.Contracts;
+using PoojaShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoojaShop.DataAccess.SQL
+{
+    public class SQLRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        internal DataContext context;
+        internal DbSet<T> dbSet;
+
+        public SQLRepository(DataContext context)
+        {
+            this.context = context;
+            this.dbSet = context.Set<T>();
+        }
+        public IQueryable<T> Collection()
+        {
+            return dbSet;
+            
+        }
+
+        public void commit()
+        {
+            context.SaveChanges();
+        }
+
+        public void Delete(string Id)
+        {
+            var t = Find(Id);
+            if (context.Entry(t).State == EntityState.Detached)
+                dbSet.Attach(t);
+
+            dbSet.Remove(t);
+        }
+
+        public T Find(string Id)
+        {
+            return dbSet.Find(Id);
+        }
+
+        public void Insert(T t)
+        {
+            dbSet.Add(t);
+        }
+
+        public void Update(T t)
+        {
+            //This is done becuase entity framework does not immediately commit the changes to db rather it
+            //does so when commit calls the SaveChanges method. Hence we need to attach the updated t to dbSet
+            //and then state it as modified so that the changes are commited to db at the time of commit.
+            dbSet.Attach(t);
+            context.Entry(t).State = EntityState.Modified;
+        }
+    }
+}
