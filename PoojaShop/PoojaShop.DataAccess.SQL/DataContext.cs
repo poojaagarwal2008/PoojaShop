a9c62be7942e29fea6fefde27ffd87029ad09f22
poojaagarwal2008 <poojaagarwal2008@gmail.com>
2021-08-17 00:53:26 +0530

    Entity Framework

---
+++ PoojaShop/PoojaShop.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using PoojaShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoojaShop.DataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        //To call the DbContext class constructor using the Default Connection string from web.config
+        public DataContext()
+            :base("DefaultConnection")
+        {
+
+        }
+
+        //In order to create database tables for the models
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+    }
+}
