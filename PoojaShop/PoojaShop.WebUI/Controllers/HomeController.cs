74d8ed2288dd93d8e221a6a21e9c636cce45c632
poojaagarwal2008 <poojaagarwal2008@gmail.com>
2021-08-19 20:13:32 +0530

    Added Product List

--- PoojaShop/PoojaShop.WebUI/Controllers/HomeController.cs
+++ PoojaShop/PoojaShop.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PoojaShop.Core.Contracts;
+using PoojaShop.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,33 @@ namespace PoojaShop.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        IRepository<Product> context;
+        IRepository<ProductCategory> productCategories;
+
+        //In order to inject the dependencies we need to use DI Container so we nweed to install Unity.MVC and Unity.Container
+        //We need to register the repositories in UnityConfig.cs file in App_Start folder under RegisterTypes method
+        public HomeController(IRepository<Product> product, IRepository<ProductCategory> productCategory)
+        {
+            context = product;
+            productCategories = productCategory;
+        }
         public ActionResult Index()
         {
-            return View();
+            List<Product> products = context.Collection().ToList();
+            return View(products);
+        }
+
+        public ActionResult Details(string Id)
+        {
+            Product product = context.Find(Id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(product);
+            }
         }
 
         public ActionResult About()
