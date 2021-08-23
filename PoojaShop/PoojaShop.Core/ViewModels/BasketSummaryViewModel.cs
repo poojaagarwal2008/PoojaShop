b269576646744b770bf6a57c9409a35868dd627a
poojaagarwal2008 <poojaagarwal2008@gmail.com>
2021-08-23 23:52:36 +0530

    Basket Service

---
+++ PoojaShop/PoojaShop.Core/ViewModels/BasketSummaryViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoojaShop.Core.ViewModels
+{
+    public class BasketSummaryViewModel
+    {
+        public int BasketCount { get; set; }
+        public decimal BasketTotal { get; set; }
+
+        BasketSummaryViewModel()
+        {
+
+        }
+
+        public BasketSummaryViewModel(int basketCount, decimal basketTotal)
+        {
+            this.BasketCount = basketCount;
+            this.BasketTotal = basketTotal;
+        }
+
+    }
+}
