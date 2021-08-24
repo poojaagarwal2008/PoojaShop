fb647bb403336c7d91b03560ebb5cfb35cd99f6c
poojaagarwal2008 <poojaagarwal2008@gmail.com>
2021-08-25 01:33:59 +0530

    Unit Testing 1

--- PoojaShop/PoojaShop.WebUI.Tests/Controllers/HomeControllerTest.cs
+++ PoojaShop/PoojaShop.WebUI.Tests/Controllers/HomeControllerTest.cs
@@ -1,21 +1,32 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PoojaShop.Core.Contracts;
+using PoojaShop.Core.Models;
+using PoojaShop.Core.ViewModels;
+using PoojaShop.WebUI.Controllers;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace PoojaShop.WebUI.Tests.Controllers
 {
     [TestClass]
-    public class HomeControllerTest
+    public class UnitTest1
     {
         [TestMethod]
-        public void Index()
+        public void IndexPageDoesReturnProducts()
         {
-            //// Arrange
-            //HomeController controller = new HomeController();
+            IRepository<Product> productContext = new Mocks.MockContext<Product>();
+            IRepository<ProductCategory> productCategoryContext = new Mocks.MockContext<ProductCategory>();
+            productContext.Insert(new Product());
+            
+            // Arrange
+            HomeController controller = new HomeController(productContext, productCategoryContext);
 
-            //// Act
-            //ViewResult result = controller.Index() as ViewResult;
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+            var viewModel = (ProductListViewModel)result.ViewData.Model;
 
-            //// Assert
-            //Assert.IsNotNull(result);
+            // Assert
+            Assert.AreEqual(1,viewModel.Products.Count());
         }
 
         [TestMethod]
